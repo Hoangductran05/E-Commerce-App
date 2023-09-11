243a83bc7d25303afce9489d51f209cd54c540d2
Hoangductran05 <hoangducdoodle@gmail.com>
2023-09-11 18:40:20 -0500

    set up home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
+import Layout from './components/layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/home'
 
 function App() {
  
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
